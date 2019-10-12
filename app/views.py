4922fcdaf95eea52d216ec37cdfc3c9ad20a59f7
sokkyyy <ndegwaraymond@gmail.com>
2019-10-12 12:50:11 +0300

    Added the dynamic route to navigate to the source news articles

--- app/views.py
+++ app/views.py
@@ -1,10 +1,20 @@
 from flask import render_template
 from app import app
 
-# Views
+# Home Page
 @app.route('/')
 def index():
     '''
     View root page function that returns the index page and its data.
     '''
-    return render_template('index.html')
+    sources = 'Hello, News Highlight'
+    return render_template('index.html', sources=sources)
+
+# Source News Articles
+@app.route('/source/<source_id>')
+def source(source_id):
+    '''
+    View source news articles page that returns the news articles of a source.
+    '''
+    # news_articles = "The news articles of a source"
+    return render_template('source.html', news_articles=source_id)
